514d475d983479379b36628865923ee1705367cf
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-01-29 14:24:58 +0800

    refactor: Use openai format

--- llm/deepseek/model.py
+++ llm/deepseek/model.py
@@ -1,9 +1,11 @@
 # More details about the model:
 #     https://github.com/deepseek-ai/DeepSeek-R1
 # export HF_ENDPOINT=https://hf-mirror.com
-from typing import Any
+from typing import Any, List
 
+from openai.types.chat import ChatCompletionUserMessageParam, ChatCompletionAssistantMessageParam
 from vllm import LLM, SamplingParams
+from vllm.entrypoints.chat_utils import ChatCompletionMessageParam
 from zerolan.data.pipeline.llm import LLMQuery, LLMPrediction, Conversation, RoleEnum
 
 from common.abs_model import AbstractModel
@@ -27,23 +29,31 @@ class DeepSeekLLMModel(AbstractModel):
 
     def predict(self, llm_query: LLMQuery):
         text, messages = self._to_deepseek_format(llm_query)
-        messages.append({'role': RoleEnum.user, 'content': text})
+        messages.append(ChatCompletionUserMessageParam(content=text, role="user"))
+        # See https://github.com/deepseek-ai/DeepSeek-R1?tab=readme-ov-file#usage-recommendations
+        # Set the temperature within the range of 0.5-0.7 (0.6 is recommended) to prevent endless repetitions or incoherent outputs.
         outputs = self._model.chat(messages, SamplingParams(temperature=0.6))
         response = outputs[0].outputs[0].text
-        messages.append({'role': RoleEnum.assistant, 'content': response})
+        messages.append(ChatCompletionAssistantMessageParam(content=response, role="assistant"))
         return self._to_pipeline_format(response, messages)
 
     def stream_predict(self, *args, **kwargs) -> Any:
         raise NotImplementedError("Not implemented yet")
 
     @staticmethod
-    def _to_pipeline_format(response: str, history: list[dict[str:str]]) -> LLMPrediction:
-        history = [Conversation(role=chat['role'], content=chat['content']) for chat in history]
+    def _to_pipeline_format(response: str, messages: List[ChatCompletionMessageParam]) -> LLMPrediction:
+        history = [Conversation(role=chat['role'], content=chat['content']) for chat in messages]
         llm_response = LLMPrediction(response=response, history=history)
         return llm_response
 
     @staticmethod
     def _to_deepseek_format(llm_query: LLMQuery):
         text = llm_query.text
-        history = [{'role': chat.role, 'content': chat.content} for chat in llm_query.history]
+        history = []
+        for chat in llm_query.history:
+            if chat.role == RoleEnum.user:
+                history.append(ChatCompletionUserMessageParam(content=chat.content, role="user"))
+            elif chat.role == RoleEnum.assistant:
+                history.append(ChatCompletionAssistantMessageParam(content=chat.content, role="assistant"))
+
         return text, history
