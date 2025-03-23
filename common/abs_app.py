f4d15ffc1e6f954038ece45f9bc71bb30d520d7a
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-03-23 18:45:20 +0800

    feat: Validate model ID

--- common/abs_app.py
+++ common/abs_app.py
@@ -3,15 +3,19 @@ from zerolan.data.data.state import AppStatusEnum, AppStatus
 
 from abc import ABC, abstractmethod
 
+from common.abs_model import AbstractModel
+
 
 class AbstractApplication(ABC):
 
-    def __init__(self, name: str):
+    def __init__(self, model: AbstractModel, name: str):
         self.name = name
         self.status = AppStatusEnum.STOPPED
         self._app = Flask(__name__)
         self._app.add_url_rule(rule=f'/{self.name}/status', view_func=self._handle_status,
                                methods=["GET", "POST"])
+        assert model, "Model should not be None."
+        self.model = model
 
     def _handle_status(self):
         return jsonify(AppStatus(status=self.status))
@@ -27,3 +31,6 @@ class AbstractApplication(ABC):
     @abstractmethod
     def _handle_stream_predict(self):
         pass
+
+    def validate_model_id(self, model_id: str):
+        return self.model.model_id == model_id
