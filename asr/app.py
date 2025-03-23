2eeefa7b0b2842306835c30d1cd319c574996b6d
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-03-23 19:27:52 +0800

    refactor: Use decorator to init ASR server

--- asr/app.py
+++ asr/app.py
@@ -10,43 +10,43 @@ from zerolan.data.pipeline.asr import ASRQuery, ASRStreamQuery, ASRPrediction
 
 class ASRApplication(AbstractApplication):
     def __init__(self, model, host: str, port: int):
-        super().__init__("asr")
+        super().__init__(model, "asr")
         self.host = host
         self.port = port
         self._app = Flask(__name__)
-        self._app.add_url_rule(rule='/asr/predict', view_func=self._handle_predict,
-                               methods=["GET", "POST"])
-        self._app.add_url_rule(rule='/asr/stream-predict', view_func=self._handle_stream_predict,
-                               methods=["GET", "POST"])
-        self._model = model
 
     def run(self):
-        self._model.load_model()
+        self.model.load_model()
+        self.init()
         self._app.run(self.host, self.port, False)
 
-    def _handle_predict(self):
-        logger.info('Request received: processing...')
+    def init(self):
+        @self._app.route('/asr/predict', methods=['POST'])
+        def handle_predict():
+            logger.info('Request received: processing...')
 
-        query: ASRQuery = web_util.get_obj_from_json(request, ASRQuery)
-        if not os.path.exists(query.audio_path):
-            audio_path = web_util.save_request_audio(request, prefix="asr")
-        else:
-            audio_path = query.audio_path
+            query: ASRQuery = web_util.get_obj_from_json(request, ASRQuery)
+            if not os.path.exists(query.audio_path):
+                audio_path = web_util.save_request_audio(request, prefix="asr")
+            else:
+                audio_path = query.audio_path
 
-        # Convert to mono channel audio file.
-        if query.channels != 1:
-            mono_audio_path = file_util.create_temp_file(prefix="asr", suffix=".wav", tmpdir="audio")
-            audio_util.convert_to_mono(audio_path, mono_audio_path, query.sample_rate)
-            query.audio_path = mono_audio_path
+            # Convert to mono channel audio file.
+            # Warning: Using ffmpeg for conversion can create performance issues
+            if query.channels != 1:
+                mono_audio_path = file_util.create_temp_file(prefix="asr", suffix=".wav", tmpdir="audio")
+                audio_util.convert_to_mono(audio_path, mono_audio_path, query.sample_rate)
+                query.audio_path = mono_audio_path
 
-        prediction: ASRPrediction = self._model.predict(query)
+            prediction: ASRPrediction = self.model.predict(query)
 
-        return jsonify(prediction.model_dump())
+            return jsonify(prediction.model_dump())
 
-    def _handle_stream_predict(self):
-        query: ASRStreamQuery = web_util.get_obj_from_json(request, ASRStreamQuery)
-        audio_data = web_util.get_request_audio_file(request).stream.read()
-        query.audio_data = audio_data
+        @self._app.route('/asr/stream-predict', methods=['POST'])
+        def handle_stream_predict():
+            query: ASRStreamQuery = web_util.get_obj_from_json(request, ASRStreamQuery)
+            audio_data = web_util.get_request_audio_file(request).stream.read()
+            query.audio_data = audio_data
 
-        prediction: ASRPrediction = self._model.stream_predict(query)
-        return jsonify(prediction.model_dump())
+            prediction: ASRPrediction = self.model.stream_predict(query)
+            return jsonify(prediction.model_dump())
